64088287ce564c9d77ef2cbe72a5260edf0394b2
Chris Knapp <chrisknappcc@gmail.com>
2026-08-21 13:34:55 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        brand: {
-          DEFAULT: '#2952e3',
-          dark: '#1c3aa9'
-        }
+        navy: { DEFAULT: '#16305C', dark: '#0E2140', light: '#2A487F' },
+        accent: { DEFAULT: '#D98C3F', dark: '#B8712E' },
+        success: '#2F8F5B',
+        ink: { 900: '#14181F', 700: '#3C4453', 500: '#6B7280', 300: '#9CA3AF' },
+        surface: '#F3EEE3',
+        card: '#FFFFFF',
+        border: '#E3DCCC'
+      },
+      fontFamily: {
+        display: ['"Sora"', 'sans-serif'],
+        sans: ['"Inter"', 'sans-serif']
       }
     }
   },
